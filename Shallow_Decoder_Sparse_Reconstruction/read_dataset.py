639463daea21880c4a0f3f9ee5f3461a861ab504
abdullah837744 <50554165+abdullah837744@users.noreply.github.com>
2024-01-28 14:04:29 -0500

    Shallow_Decoder_Sparse_Reconstruction

---
+++ Shallow_Decoder_Sparse_Reconstruction/read_dataset.py
@@ -0,0 +1,49 @@
+##### Data Reading #######
+
+
+import numpy as np
+from scipy.io import loadmat
+
+
+#******************************************************************************
+# Read in data
+#******************************************************************************
+def data_from_name(name):
+    if name == 'flow_cylinder':
+        return flow_cylinder()
+
+    else:
+        raise ValueError('dataset {} not recognized'.format(name))
+
+
+def rescale(Xsmall, Xsmall_test):
+    #******************************************************************************
+    # Rescale data
+    #******************************************************************************
+    Xmin = Xsmall.min()
+    Xmax = Xsmall.max()
+
+    Xsmall = ((Xsmall - Xmin) / (Xmax - Xmin))
+    Xsmall_test = ((Xsmall_test - Xmin) / (Xmax - Xmin))
+
+    return Xsmall, Xsmall_test
+
+
+
+def flow_cylinder():
+    X = np.load('data/flow_cylinder.npy')
+    print(X.shape)
+
+
+    # Split into train and test set
+    Xsmall = X[0:100, :, :]
+    t, m, n = Xsmall.shape
+
+    Xsmall = Xsmall.reshape(100, -1)
+
+    Xsmall_test = X[100:151, :, :].reshape(51, -1)
+
+    #******************************************************************************
+    # Return train and test set
+    #******************************************************************************
+    return Xsmall, Xsmall_test, m, n
